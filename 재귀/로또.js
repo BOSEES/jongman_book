75ee601cda81ffc08640173da1a27e2a5606a042
김영일 <rladuddlf3@naver.com>
2021-03-07 17:53:43 +0900

    재귀_로또

--- 재귀/로또.js
+++ 재귀/로또.js
@@ -1,8 +1,21 @@
   let input = [7,1,2,3,4,5,6,7];
   let k = input.shift();
 
-  function sol() {
+  function sol(arr, selectNumber) {
+    let result = [];
+    if (selectNumber === 1) return arr.map((value) => [value]);
     
-  }
+    arr.forEach((fixed, index, origin) => {
+      const rest = origin.slice(index + 1);
+      const combinations = sol(rest, selectNumber - 1);
+      const attached = combinations.map((combination) => [fixed, ...combination]);
+      result.push(...attached);
+    })
 
-  console.log(sol());
+    return result;
+  }
+  
+  
+  console.log(sol(input, 6));
+
+
