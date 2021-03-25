93e52ceef326717f819309fae71b5aae63151c75
김영일 <rladuddlf3@naver.com>
2021-03-25 17:42:11 +0900

    DFS - 동전 교환

---
+++ 재귀/동전_교환.js
@@ -0,0 +1,21 @@
+let arr=[1, 2, 5];
+
+function solution(m, arr) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  function DFS(L, sum) {
+    if (sum > m) return;
+    if (L >= answer) return;
+    if (sum == m) {
+      console.log(sum, L)
+      answer = Math.min(answer, L);
+    } else {
+      for(let i = 0; i < arr.length; i++) {
+        DFS(L + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0,0);
+  return answer;
+}
+
+console.log(solution(15, arr));
