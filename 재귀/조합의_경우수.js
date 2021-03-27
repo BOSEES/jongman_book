a32679c9ad27d07425ce6e0a3a1c8477359b26e7
김영일 <rladuddlf3@naver.com>
2021-03-27 16:41:27 +0900

    재귀_수들의 조합

---
+++ 재귀/조합의_경우수.js
@@ -0,0 +1,16 @@
+
+function solution(n, r) {
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+  
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (r == 0 || n == r) return 1;
+    else return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+  }
+
+  answer = DFS(n, r);
+  return answer;
+}
+
+console.log(solution(33, 19));
