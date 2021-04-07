76f2ce12cde1aa437417b35cc364e89eff254dbc
김영일 <rladuddlf3@naver.com>
2021-04-07 15:08:26 +0900

    치즈

--- 그래프와_탐색/빙산.js
+++ 그래프와_탐색/빙산.js
@@ -5,9 +5,7 @@ let input = `5 7
 0 7 6 2 4 0 0
 0 0 0 0 0 0 0`
 
-let arr = input.split("\n").map((e) =>{
-  e.split(" ").map((x) => x = parseInt(x, 10))
-});
+let arr = input.split("\n").map((e) => e.split(" ").map((x) => x = parseInt(x, 10)));
 
 function solution(ice) {
   let answer = 0;
@@ -16,36 +14,37 @@ function solution(ice) {
   let m = mn[1] // 가로
   let dx = [-1,0,1,0];
   let dy = [0,1,0,-1];
-
-  for (let y = 1; y <= 10;  y++) {
-    for (let i = 1; i < n - 1; i++) {
-      for (let j = 1; j < m - 1; j++) {
-        let count = 0;
-        if (ice[i][j] > 0) {
-          for (let k = 0; k < 4; k++) {
-            let nx = i + dx[k];
-            let ny = j + dy[k];
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m && ice[nx][ny] == 0) {
-              count++
-            } 
+  let flag = 1;
+  
+    while(flag) {
+      answer++;
+      for (let y = 1; y <= 10;  y++) {
+        for (let i = 1; i < n - 1; i++) {
+          for (let j = 1; j < m - 1; j++) {
+            let count = 0;
+            if (ice[i][j] > 0) {
+              for (let k = 0; k < 4; k++) {
+                let nx = i + dx[k];
+                let ny = j + dy[k];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && ice[nx][ny] == 0) {
+                  count++
+                } 
+              }
+              if(ice[i][j] - count == 0) ice[i][j] = -1
+              else ice[i][j] -= count; 
+            }
           }
-          if(ice[i][j] - count == 0) ice[i][j] = -1
-          else ice[i][j] -= count; 
         }
-      }
+    
+        for (let i = 1; i < n - 1; i++) {
+          for (let j = 1; j < m - 1; j++) {
+            if(ice[i][j] < 0) ice[i][j] = 0;
+          }  
+        }
     }
-
-    for (let i = 1; i < n - 1; i++) {
-      for (let j = 1; j < m - 1; j++) {
-        if(ice[i][j] < 0) ice[i][j] = 0;
-      }  
-    }
-
-    // if (thawing(ice) > 1) {
-    //   console.log("aa")
-    //   answer = y;
-    //   break
-    // }
+  }
+  if (thawing(ice)) {
+    flag = 0;
   }
   return answer;
 }
@@ -79,6 +78,10 @@ function thawing(ice) {
     }
   }
   
-  return count2;
+  if (count2 > 1) {
+    return true;
+  }
+
+  return false;
 }
 console.log(solution(arr));
