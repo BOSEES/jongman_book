564eb713b56a8cb369e8e3e4e18bda7085170c9b
bosees <rladuddlf3@naver.com>
2021-10-05 11:53:13 +0900

    굿 코드

--- 자바스크립트_초급/트리/부분집합의합.js
+++ 자바스크립트_초급/트리/부분집합의합.js
@@ -1,32 +1,55 @@
 let arr=[1, 3, 5, 6, 7, 10];
 
-function solution(data) {
+//Bad code.
+// function solution(data) {
+//   let answer = "NO";
+//   let ch = Array.from({length: data.length}, () => 0);
+//   function DFS(index) {
+//     if(index >= data.length) {
+//       let temp1 = 0;
+//       let temp2 = 0;
+//       for (let i = 0; i < data.length; i++) {
+//         if (ch[i] === 1) {
+//           temp1 += data[i];
+//         } else {
+//           temp2 += data[i];
+//         }
+//       }
+//       if (temp1 === temp2) {
+//         answer = "YES";
+//         return;
+//       }
+//     } else {
+//       ch[index] = 1;
+//       DFS(index + 1);
+//       ch[index] = 0;
+//       DFS(index + 1);
+//     }
+//   }
+
+//   DFS(0);
+//   return answer;
+// }
+
+//Good code.
+function solution (data) {
   let answer = "NO";
-  let sumArray = [];
-  let ch = Array.from({length: data.length}, () => 0);
-  function DFS(index) {
-    if(index >= data.length) {
-      let temp = 0;
-      for (let i = 0; i < data.length; i++) {
-        if (ch[i] === 1) {
-          temp += data[i];
-        }
+  let total = data.reduce((a, b) => a + b, 0);
+  let flag = 0;
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L >= data.length) {
+      if(total/2 === sum) {
+        answer = "YES";
+        flag = 1;
       }
-      sumArray.push(temp);
     } else {
-      ch[index] = 1;
-      DFS(index + 1);
-      ch[index] = 0;
-      DFS(index + 1);
+      DFS(L+1, sum + data[L]);
+      DFS(L+1, sum);
     }
   }
 
-  DFS(0);
-
-  sumArray.sort((a, b) => a - b);
-  for (let i = 0; i < sumArray.length - 1; i++) {
-    if (sumArray[i] === sumArray[i + 1]) return true;
-  }
+  DFS(0,0);
   return answer;
 }
 
