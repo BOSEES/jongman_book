71b959871ec3f403a75f3f2a14b4fcf703f4810d
bosees <rladuddlf3@naver.com>
2021-10-09 15:29:47 +0900

    키로거

--- 제로베이스/키로거.js
+++ 제로베이스/키로거.js
@@ -3,40 +3,67 @@ let input = `2
 ThIsIsS3Cr3t`;
 
 input = input.split("\n");
-function sol(data) {
-  let n = data.shift();
-  let answer = [];
+// function sol(data) {
+//   let n = data.shift();
+//   let answer = [];
 
-  for(let i = 0; i < n; i++) {
-    let temp = [];
-    let str = data[i];
-    let cursor = 0;
+//   for(let i = 0; i < n; i++) {
+//     let temp = [];
+//     let str = data[i];
+//     let cursor = 0;
     
-    for (let j = 0; j < str.length; j++) {
-      if (str[j] === "<") {
-        if (cursor > 0)  {
-          cursor--;
+//     for (let j = 0; j < str.length; j++) {
+//       if (str[j] === "<") {
+//         if (cursor > 0)  {
+//           cursor--;
+//         }
+//       } else if (str[j] === ">") {
+//         if (cursor < temp.length - 1) {
+//           cursor++;
+//         }
+//       } else if (str[j] === "-") {
+//         if (temp.length - 1 === cursor) {
+//           temp.pop();
+//           cursor--;
+//         } else if (cursor !== 0) {
+//           temp.splice(cursor - 1, cursor);
+//           cursor--;
+//         }
+//       } else {
+//         temp.push(str[j]);
+//         cursor++;
+//       }
+//     }
+//     answer.push(temp.join(""));
+//   }
+//   return answer.join("\n");
+// }
+
+function sol(data) {
+  const n = data.shift();
+
+  for (let i = 0; i < n; i++) {
+    let left = [];
+    let right = [];
+    for (let j = 0; j < data[i].length; j++) {
+      if (data[i][j] === "<") {
+        if (left.length > 0) {
+          right.push(left.pop());
         }
-      } else if (str[j] === ">") {
-        if (cursor < temp.length - 1) {
-          cursor++;
+      } else if (data[i][j] === ">") {
+        if (right.length > 0) {
+          left.push(right.pop());
         }
-      } else if (str[j] === "-") {
-        if (temp.length - 1 === cursor) {
-          temp.pop();
-          cursor--;
-        } else if (cursor !== 0) {
-          temp.splice(cursor - 1, cursor);
-          cursor--;
+      } else if (data[i][j] === "-") {
+        if(left.length > 0) {
+          left.pop()
         }
       } else {
-        temp.push(str[j]);
-        cursor++;
+        left.push(data[i][j]);
       }
     }
-    answer.push(temp.join(""));
+    console.log(left.join(""));
   }
-  return answer.join("\n");
 }
 
 console.log(sol(input))
